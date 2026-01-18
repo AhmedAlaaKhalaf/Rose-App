b23f6d1dad5a90d95eb1d831e843997ec4649916
Ahmed-Fathy-Abkarenoo <a_abomosalam@hotmail.com>
2026-01-19 01:15:29 +0200

    chore(fonts):add missing import for fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,6 @@ import { notFound } from "next/navigation";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { Toaster } from "@/components/ui/sonner";
 import localFont from "next/font/local";
-
 type LocaleProps = {
   children: React.ReactNode;
   params: { locale: string };
