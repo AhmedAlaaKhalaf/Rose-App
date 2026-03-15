15522468a1215eb8a5d00359327c70f4b4b16689
Ahmed Alaa Khalaf <ahmed.khalaf1611@gmail.com>
2026-03-15 13:59:17 +0200

    fix(dashboard): move dashboard files under one directory.

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import TopSection from "./_components/top-section";
+import AnalyticsSection from "./_components/analytics-section";
 
 export default function DashboardPage() {
   return (
-    <div className="pr-4 pl-4">
+    <div className="pr-4 pl-4 flex flex-col gap-6">
       <TopSection />
+      <AnalyticsSection />
     </div>
   );
 }
